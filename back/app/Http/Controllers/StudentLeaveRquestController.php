a2239f26a96232d641579b84572e3533c3eedc45
SREYMAO VORN <sreymao.vorn@pnc.lan>
2022-08-16 14:53:13 +0700

    #10 CRUD and validation student and student leave request

---
+++ back/app/Http/Controllers/StudentLeaveRquestController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\StudentLeaveRquest;
+use Illuminate\Http\Request;
+
+class StudentLeaveRquestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        return StudentLeaveRquest:: get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $request->validate([
+            'start_date' => [
+                'required',
+                'date_format:Y-m-d',    
+        ],
+            'end_date' => [
+                'required',
+                'date_format:Y-m-d',    
+        ],
+            'reason' => 'required',
+           
+            'request_date' => [
+                'required',
+                'date_format:Y-m-d',    
+        ],
+        ]);
+        $studentLeaveRequest = new StudentLeaveRquest();
+        $studentLeaveRequest->start_date = $request->start_date;
+        $studentLeaveRequest->end_date = $request->end_date;
+        $studentLeaveRequest->reason = $request->reason;
+        $studentLeaveRequest->leave_type = $request->leave_type;
+        $studentLeaveRequest->duration = $request->duration;
+        $studentLeaveRequest->status = $request->status;
+        $studentLeaveRequest->request_date = $request->request_date;
+        $studentLeaveRequest->student_id= $request->student_id;
+        $studentLeaveRequest->save();
+        return response()->json(['message:'=>'create studentLeaveRequest successfully']);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\StudentLeaveRquest  $studentLeaveRquest
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        return StudentLeaveRquest::where('id', $id)->get();
+        
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\StudentLeaveRquest  $studentLeaveRquest
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request,$id)
+    {
+        //
+        $request->validate([
+            'start_date' => [
+                'required',
+                'date_format:Y-m-d',    
+        ],
+            'end_date' => [
+                'required',
+                'date_format:Y-m-d',    
+        ],
+            'reason' => 'required',
+           
+            'request_date' => [
+                'required',
+                'date_format:Y-m-d',    
+        ],
+        ]);
+        
+        $studentLeaveRequest = StudentLeaveRquest::findOrFail($id);
+        $studentLeaveRequest->start_date = $request->start_date;
+        $studentLeaveRequest->end_date = $request->end_date;
+        $studentLeaveRequest->reason = $request->reason;
+        $studentLeaveRequest->leave_type = $request->leave_type;
+        $studentLeaveRequest->duration = $request->duration;
+        $studentLeaveRequest->status = $request->status;
+        $studentLeaveRequest->request_date = $request->request_date;
+        $studentLeaveRequest->student_id= $request->student_id;
+        $studentLeaveRequest->save();
+        return response()->json(['message:'=>'update studentLeaveRequest successfully']);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\StudentLeaveRquest  $studentLeaveRquest
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        return StudentLeaveRquest::destroy($id);
+    }
+}
--- back/routes/api.php
+++ back/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\StudentLeaveRquestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/student', [StudentController::class, 'store']);
+Route::get('/student', [StudentController::class, 'index']);
+Route::get('/student/{id}', [StudentController::class, 'show']);
+Route::put('/student/{id}', [StudentController::class, 'update']);
+Route::delete('/student/{id}', [StudentController::class, 'destroy']);
+
+Route::post('/studentleaveRequest', [StudentLeaveRquestController::class, 'store']);
+Route::get('/studentleaveRequest', [StudentLeaveRquestController::class, 'index']);
+Route::get('/studentleaveRequest/{id}', [StudentLeaveRquestController::class, 'show']);
+Route::put('/studentleaveRequest/{id}', [StudentLeaveRquestController::class, 'update']);
+Route::delete('/studentleaveRequest/{id}', [StudentLeaveRquestController::class, 'destroy']);
