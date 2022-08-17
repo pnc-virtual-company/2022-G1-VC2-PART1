6eb938229dbbfeefd747407e138a21eee4f756a8
Vansao.HANG <vansao.hang@pnc.lan>
2022-08-17 12:56:38 +0700

    #12 Filter list leave of student

--- front/src/components/student/ListAllLeave.vue
+++ front/src/components/student/ListAllLeave.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="cotainer_filter">
+<section>
+    <div class="cotainer_filter">
     <div class="status_type">
       <span>Status Type: </span>
       <select class="select_controll" v-model="status_type">
@@ -50,6 +51,7 @@
       </tbody>
     </table>
   </div>
+</section>
 </template>
 
 <script>
