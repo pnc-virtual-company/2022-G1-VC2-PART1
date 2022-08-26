24b53bc6af7305481c25b80083b10781019328aa
Vansao.HANG <vansao.hang@pnc.lan>
2022-08-26 11:54:59 +0700

    #32 send email

--- back/routes/api.php
+++ back/routes/api.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\StudentLeaveRquestController;
+use App\Http\Controllers\TeacherController;
+use App\Http\Controllers\MailController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\StudentController;
-use App\Http\Controllers\TeacherController;
-use App\Http\Controllers\StudentLeaveRquestController;
-use Laravel\Sanctum\Sanctum;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,7 +16,7 @@ use Laravel\Sanctum\Sanctum;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -25,18 +26,18 @@ Route::post('/student', [StudentController::class, 'store']);
 Route::get('/student', [StudentController::class, 'index']);
 Route::get('/student/{id}', [StudentController::class, 'show']);
 Route::delete('/student/{id}', [StudentController::class, 'destroy']);
-
 Route::post("addstudent", [StudentController::class, "createAccount"]);
+
 Route::post("login", [StudentController::class, "userLogin"]);
 
 //Student private routes
-Route::group(['middleware'=>['auth:sanctum']],function(){
+Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post("logout", [StudentController::class, "logout"]);
     Route::get("userLogin", [StudentController::class, "user"]);
     Route::apiresource("studentleaveRequest", StudentLeaveRquestController::class);
     Route::put('/student/password/update/{id}', [StudentController::class, 'updatePassword']);
-});
 
+});
 
 /*
 ================= Teacher =====================
@@ -47,3 +48,4 @@ Route::get('/teachers', [TeacherController::class, 'index']);
 Route::get('/teachers/{id}', [TeacherController::class, 'show']);
 Route::put('/teachers/{id}', [TeacherController::class, 'update']);
 Route::delete('/teachers/{id}', [TeacherController::class, 'destroy']);
+Route::get('/sendMail',[MailController::class,"sendEmail"]);
