0b9d83396959b00c69e01b857049d3c67f9229b9
SavouertPheap <savouert.pheap@student.passerellesnumeriques.org>
2022-09-01 10:30:09 +0700

    #40 Admin change password

--- front/src/components/teacher/TeacherProfileView.vue
+++ front/src/components/teacher/TeacherProfileView.vue
@@ -275,18 +275,21 @@ ul li {
   display: flex;
 }
 .card_profile {
-  position: relative;
-  height: 48px;
+  border-radius: 90%;
+  padding: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
-.card .card_profile .img-profile {
-  width: 130px;
-  height: 130px;
-  border-radius: 1000px;
-  position: absolute;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  border: 4px solid orange;
-  box-shadow: 0 0 20px #ead2d246;
+.img-profile {
+  margin: auto;
+  box-shadow: rgba(6, 24, 44, 0.4) 0px 0px 0px 2px,
+    rgba(6, 24, 44, 0.65) 0px 4px 6px -1px,
+    rgba(255, 255, 255, 0.08) 0px 1px 0px inset;
+  width: 6rem;
+  height: 6rem;
+  border-radius: 100%;
+  border:none;
 }
 
 .fa-camera {
